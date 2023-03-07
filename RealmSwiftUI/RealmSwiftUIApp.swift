0f042a180c8d815e1ae01af5329ffabf8cc1949b
hafizhMo <hafizh.hizb@gmail.com>
2023-03-07 14:08:30 +0700

    Initial Commit

---
+++ RealmSwiftUI/RealmSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSwiftUIApp.swift
+//  RealmSwiftUI
+//
+//  Created by Hafizh Mo on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RealmSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
